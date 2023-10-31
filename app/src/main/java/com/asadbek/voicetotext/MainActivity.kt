01986ed665e320efe09a699e7660688700e4b930
Dominic <98159825+DominicDeveloper@users.noreply.github.com>
2023-10-31 14:01:25 +0500

    voice

---
+++ app/src/main/java/com/asadbek/voicetotext/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.asadbek.voicetotext
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Toast
+import com.asadbek.voicetotext.databinding.ActivityMainBinding
+import android.R.attr
+import android.content.Context
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var flashLightStatus: Boolean = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding  = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startDictation.setOnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ru")
+            intent.putExtra("android.speech.extra.EXTRA_ADDITIONAL_LANGUAGES", arrayOf("ru"))
+
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT,getString(R.string.speech_prompt))
+
+            startActivityForResult(intent,1)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            1 ->{
+                if (resultCode == RESULT_OK && null != attr.data){
+                    val result: ArrayList<String> = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)!!
+
+                    binding.textOutput.setText(result[0])
+                    val thetext:String = binding.textOutput.text.toString().lowercase()
+                    if (thetext == "выключай фонарики" || thetext == "включай фонар" || thetext == "включай фонара" || thetext == "включай спичку"){
+                        openFlash()
+                    }else if (thetext == "отключай спичку" || thetext == "отключайте спичку" || thetext == "спичку отключай"){
+                        closeFlash()
+                    }
+                }
+            }
+        }
+    }
+
+    // flashlight turn on
+    private fun openFlash(){
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[0]
+        if (!flashLightStatus) {
+            try {
+                cameraManager.setTorchMode(cameraId, true)
+                flashLightStatus = true
+
+            } catch (e: CameraAccessException) {
+            }
+        } else {
+            try {
+                cameraManager.setTorchMode(cameraId, false)
+                flashLightStatus = false
+            } catch (e: CameraAccessException) {
+            }
+        }
+    }
+
+    // flashlight turn off
+    private fun closeFlash(){
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[0]
+        if (!flashLightStatus) {
+            try {
+                cameraManager.setTorchMode(cameraId, false)
+                flashLightStatus = false
+
+            } catch (e: CameraAccessException) {
+            }
+        } else {
+            try {
+                cameraManager.setTorchMode(cameraId, false)
+                flashLightStatus = false
+            } catch (e: CameraAccessException) {
+            }
+        }
+    }
+}
